6333603c85ca09b156321b20798b7ca7ac3b5803
Pushpak Jalan <pushpakjalan02@gmail.com>
2019-11-06 23:29:52 +0530

    Changed Problem A 2 in Assignment 6

--- Assignment_6/Problem_A_2.c
+++ Assignment_6/Problem_A_2.c
@@ -12,7 +12,7 @@ int main(){
 	int shm_id;
 
 	key = ftok("/home/pushpakjalan/Desktop/OS/Assignment_6/a_1.txt", 'A');
-	shm_id = shmget(key, 100, 0777 | IPC_CREAT);
+	shm_id = shmget(key, 100, 0);
 	sh_mem = shmat(shm_id, NULL, 0);
 	
 	printf("Process ID of Sibling: %d\n", sh_mem -> pid);
