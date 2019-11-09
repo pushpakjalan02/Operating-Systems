99f751d6315aaf0a9395dabadb46904f8d634095
Pushpak Jalan <pushpakjalan02@gmail.com>
2019-11-09 16:52:42 +0530

    Learnt semget

---
+++ Assignment_7/Program2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/sem.h>
+
+int main(int argc, char* argv[]){
+	key_t key;
+	int sem_set_identifier;
+
+	key = ftok("/home/pushpakjalan/Desktop/OS/Assignment_7/file.txt", 1);
+	sem_set_identifier = semget(key, 1, 0666 | 0);
+
+}
